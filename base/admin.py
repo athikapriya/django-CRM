e12e9e365e989e8f3dc65c28973a2c49b9aa0610
Athika Priya <athikapriya1997@gmail.com>
2026-01-03 00:58:44 +0600

    Add Tag model

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Customer, Product
+from .models import Customer, Tag, Product
 
 admin.site.register(Customer)
+admin.site.register(Tag)
 admin.site.register(Product)
