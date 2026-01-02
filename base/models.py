2710067d7c80cc9653ecb8a738229b14363c91d4
Athika Priya <athikapriya1997@gmail.com>
2026-01-03 00:51:44 +0600

    Add Products model

--- base/models.py
+++ base/models.py
@@ -9,3 +9,20 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+
+    CATEGORY =(
+        ("Indoor", "Indoor"),
+        ("Outdoor", "Outdoor")
+    )
+
+    name = models.CharField(max_length=100, null=True)
+    price = models.FloatField(max_length=200, null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description = models.TextField(max_length=200, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
