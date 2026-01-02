24a3fbb11cd251f7ddd3e895c74e56b96c92f637
Athika Priya <athikapriya1997@gmail.com>
2026-01-03 00:36:34 +0600

    Add Customer model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    phone = models.CharField(max_length=50, blank=True)
+    email = models.CharField(max_length=100, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
