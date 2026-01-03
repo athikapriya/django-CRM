2c56bb67a55d4791d814cfac6166cdcfe94ad293
Athika Priya <athikapriya1997@gmail.com>
2026-01-03 17:30:36 +0600

    Render database data in status.html

--- base/views.py
+++ base/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from .models import *
 
 
 # homepage view
 def homepage(request):
-    context = {}
+    orders = Order.objects.all()
+    total_orders = orders.count()
+    total_pending = orders.filter(status="Pending").count()
+    total_delivered = orders.filter(status="Delivered").count()
+    context = {
+        "total_orders" : total_orders,
+        "total_pending" : total_pending,
+        "total_delivered" : total_delivered
+    }
     return render(request, 'base/homepage.html', context)
 
 
