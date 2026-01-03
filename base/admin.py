82a00dd4691caaa2f66b07252ba0aac95bdfcfc1
Athika Priya <athikapriya1997@gmail.com>
2026-01-03 17:12:32 +0600

    Add Order model

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Customer, Tag, Product
+from .models import Customer, Tag, Product, Order
 
 admin.site.register(Customer)
 admin.site.register(Tag)
 admin.site.register(Product)
+admin.site.register(Order)
--- base/models.py
+++ base/models.py
@@ -36,3 +36,21 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+
+class Order(models.Model):
+
+    STATUS = (
+        ("Pending", "Pending"),
+        ("Out for delivery", "Out for delivery"),
+        ("Delivered", "Delivered")
+    )
+
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.product.name
