33af1708537c8c37d19a5f7b94fe8f50255f2f22
Athika Priya <athikapriya1997@gmail.com>
2026-01-12 00:37:55 +0600

    assign new users to customer group and protect homepage

--- base/views.py
+++ base/views.py
@@ -6,7 +6,8 @@ from .filters import OrderFilter
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from .decorators import unauthenticated_user, allowed_users
+from .decorators import unauthenticated_user, allowed_users, admin_only
+from django.contrib.auth.models import Group
 
 
 
@@ -18,7 +19,11 @@ def registerPage(request):
     if request.method == "POST":
         form = CreateUserForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            username = form.cleaned_data.get("username")
+
+            group = Group.objects.get(name="customer")
+            user.Group.add(group)
             messages.success(request, "Account created successfully. You can now login!" )
             return redirect("login")
 
@@ -32,25 +37,21 @@ def registerPage(request):
 # login page views
 @unauthenticated_user
 def loginPage(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
 
-    if request.user.is_authenticated:
-        return redirect("homepage")
-    else:
-        if request.method == "POST":
-            username = request.POST.get("username")
-            password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
 
-            user = authenticate(request, username=username, password=password)
-
-            if user is not None:
-                login(request, user)
-                return redirect("homepage")
-            else:
-                messages.info(request, "Username or password is incorrect !")
-        context = {
-
+        if user is not None:
+            login(request, user)
+            return redirect("homepage")
+        else:
+            messages.info(request, "Username or password is incorrect !")
+    context = {
+        
         }
-        return render(request, 'base/login.html', context)
+    return render(request, 'base/login.html', context)
 
 
 
@@ -72,7 +73,7 @@ def unauthorized(request):
 
 # homepage view
 @login_required(login_url="login")
-
+@admin_only
 def homepage(request):
     customers = Customer.objects.all()
     orders = Order.objects.all()
