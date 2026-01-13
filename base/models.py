418c4974888b8ed5bf4cb1ea43f83ab33eb387e2
Athika Priya <athikapriya1997@gmail.com>
2026-01-13 22:47:27 +0600

    Add user relation to Customer model

--- base/models.py
+++ base/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Customer(models.Model):
+    user = models.OneToOneField(User, blank=True, null=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=100, null=True)
     phone = models.CharField(max_length=50, blank=True)
     email = models.CharField(max_length=100, blank=True)
