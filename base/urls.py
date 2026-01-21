e30a5482523df00d6d3f8a9b8825b45bb2eed0ee
Athika Priya <athikapriya1997@gmail.com>
2026-01-21 21:42:47 +0600

    Refactor user-related URLs to be username-based

--- base/urls.py
+++ base/urls.py
@@ -9,12 +9,13 @@ urlpatterns = [
     path('unauthorized/', views.unauthorized, name='unauthorized'),
 
     path("", views.homepage, name="homepage"),
-    path("user/", views.userProfile, name="user"),
-    path("account_settings/", views.accountSettings, name="settings"),
     
     path('products/', views.products, name="products"),
     path('customer/<str:pk>', views.customer, name="customer"),
     path("create_order/<str:pk>/", views.CreateOrder, name="create_order"),
     path("update_order/<str:pk>/", views.UpdateOrder, name="update_order"),
     path("delete_order/<str:pk>/", views.DeleteOrder, name="delete_order"),
+
+    path("<str:username>/account_settings/", views.accountSettings, name="settings"),
+    path("<str:username>/", views.userProfile, name="user_profile"),
 ]
