1c05cdf4ffa3469cddf8c7f01795d0aa6b31a16c
Athika Priya <athikapriya1997@gmail.com>
2026-01-25 19:25:08 +0600

    Add custom templates for Django password reset views

--- base/urls.py
+++ base/urls.py
@@ -18,10 +18,10 @@ urlpatterns = [
     path("update_order/<str:pk>/", views.UpdateOrder, name="update_order"),
     path("delete_order/<str:pk>/", views.DeleteOrder, name="delete_order"),
 
-    path("reset_password/", auth_views.PasswordResetView.as_view(), name="password_reset"),
-    path("reset_password_sent/", auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path("reset_password_complete/", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    path("reset_password/", auth_views.PasswordResetView.as_view(template_name="base/password_reset.html"), name="password_reset"),
+    path("reset_password_sent/", auth_views.PasswordResetDoneView.as_view(template_name="base/reset_password_sent.html"), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="base/password_reset_confirm.html"), name='password_reset_confirm'),
+    path("reset_password_complete/", auth_views.PasswordResetCompleteView.as_view(template_name="base/password_reset_complete.html"), name="password_reset_complete"),
 
     path('password_reset_otp/', views.request_password_otp, name='password_reset_otp'),
     path('password_reset_verify/', views.verify_otp, name='verify_otp'),
